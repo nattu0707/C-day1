34bdf1e94c460ecb75dc23e918c23be62a10356f
ryuki25695 <j2200075@gunma-u.ac.jp>
2025-04-10 20:35:54 +0900

    a

---
+++ C++/c++-1.cpp
@@ -0,0 +1,21 @@
+//【問題 2: メンバ関数をもつ構造体】
+//「Rectangle」という構造体を作成してください。
+
+//構造体には以下のメンバ変数を含めます。
+//width（double）
+//height（double）
+
+//また、以下の2つのメンバ関数を定義してください。
+//area()：面積（幅 × 高さ）を返す
+//perimeter()：周囲長（2×(幅＋高さ)）を返す
+//これらの関数を利用して、長方形の面積と周囲長を表示するプログラムを作成してください。
+
+#include<iostream>
+using namespace std;
+struct Rectangle{
+    double width;
+    double height;
+};
+double area(width,height){
+
+}
